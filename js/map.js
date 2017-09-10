b98f91fded604d822242aae614f5c093a280b0d8
Tyler McDonough <tcmcd1337@gmail.com>
2017-09-10 00:26:17 +0000

    Delete buttons added to markers on MapSnap. + Successful test of header modularity

--- js/map.js
+++ js/map.js
@@ -1,24 +1,28 @@
 var map;
-var marker;
+var tempMarker;
 var infoWindow;
 var addressInput;
 var geocodeResponse;
 var geocodeLatLng;
+
+var counter = -1;
+var markers = [];
 
 
 function initialize() {
   var mapOptions = {
     center: new google.maps.LatLng(41.05, -73.8),
     zoom: 8,
-    mapTypeId: google.maps.MapTypeId.SATELLITE
+    mapTypeId: google.maps.MapTypeId.SATTELITE
   };
    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  
-    
 }
+
+//INITIALIZE MAP when DOM loads
 google.maps.event.addDomListener(window, "load", initialize);
 
 
+//
 function searchAddress() 
 {
 
@@ -34,28 +38,27 @@ function searchAddress()
     
     if (status == google.maps.GeocoderStatus.OK) 
     {
-        
+    counter++;    
     geocodeResponse = results;
       
     geocodeLatLng = results[0].geometry.location; // reference LatLng value
      
       createMarker(geocodeLatLng);
-      
      
       map.panTo( geocodeLatLng); 
       map.setZoom(12);
-      
-      
-      //map.
-    
+    }
+    else if (status == google.maps.GeocoderStatus.INVALID_REQUEST){
+        // warning message
+    alert("..?\n\n If one searches for nothing, "
+    +"can it ever truly be found?\n\nPlease consider this before searching."  );
 
-     
-
-    }else { // if status value is not equal to "google.maps.GeocoderStatus.OK"
+    }
+    else { // if status value is not equal to "google.maps.GeocoderStatus.OK"
 
     // warning message
     alert("Our survey says: " + status + "\n\n'"
-    +addressInput +"' is not a real place.\nPlease Try again."  );
+    +addressInput +"' is not a 'Google-able' place.\nPlease Try again."  );
 
   }
   });
@@ -68,12 +71,11 @@ function createMarker(latlng)
    // if(marker != undefined && marker != ''){
    //  infoWindow.close(map,marker);
    // }
-     
-     
-    marker = new google.maps.Marker({
+    tempMarker = new google.maps.Marker({
       map: map,
       position: latlng,
-      title: ("Found: '" + addressInput + "'")
+      title: ("Found: '" + addressInput + "'"),
+      id: counter
     });
 
 
@@ -85,13 +87,16 @@ function createMarker(latlng)
       '<b id="firstHeading" class="firstHeading">Found:  \' </b>'+ addressInput  + '<b>\'</b>'
       +'<hr style="padding:none;">'
       +'<div id="bodyContent">'
-       +'<p><b>Address</b>: '+ geocodeResponse[0].formatted_address  + '</p>'
+       +'<p><b>Address!</b>: '+ geocodeResponse[0].formatted_address  + '</p>'
        +'<p><b>Lat/Long</b>: '+ geocodeLatLng  + '</p>'
       +'</div>'
+    +'<btn class="mapBtn" onclick="removeMarker('+ counter +');">Remove Marker</btn>' 
+     
+     
      +'</div>';
 
-        //WAIT FOR CLICK (LISTENER)
-       marker.addListener('click', function() {
+        //On marker CLICK, create and display the info Window
+       tempMarker.addListener('click', function() {
                 infoWindow = new google.maps.InfoWindow({
                 content: contentString
                  });
@@ -100,7 +105,12 @@ function createMarker(latlng)
                 infoWindow.open(map, this);
               });
               
-            
-
-
+            markers.push(tempMarker);
 }
+
+function removeMarker(markerId)
+{
+        var markerToBeRemoved = markers[markerId];
+        google.maps.event.clearListeners(markerToBeRemoved, 'click');
+        markerToBeRemoved.setMap(null);
+      }
